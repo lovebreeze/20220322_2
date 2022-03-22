b8c392e73bffcfe6c8dafc86b24027db560e336f
junheenim <101177178+junheenim@users.noreply.github.com>
2022-03-22 19:49:35 +0900

    ss

--- 20220322_2/소스.cpp
+++ 20220322_2/소스.cpp
@@ -31,3 +31,49 @@ int main()
 	return 0;
 }
 
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+
+	scanf_s("%d %d", &a, &b);
+	int max = 0;
+	for (int i = 1; i <= a; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			if (max < i)
+			{
+				max = i;
+			}
+		}
+	}
+
+	printf("최대공약수는 %d ", max);
+
+	return 0;
+}
+
+//for 문
+
+//for (int i = 1; i <= 10; i++)
+//{
+//	if (i % 3 == 0)
+//	{
+//		printf("\n(3의 배수임)");
+//		continue;
+//	}
+//	printf("\n%d", i);
+//}
+
+//쉬프트 연산자
+
+//int memory = 3; // 0000 0011 
+////비트를 3번 오른쪽으로 이동 0001 1000
+//printf("%d\n", memory <<= 3);
+//printf("%d\n", memory);
+//printf("%d\n", memory >>= 5);
+
+//최대 공약수 
+// 변수 2개에 값을 설정해서 최대 공약수 구하기;
